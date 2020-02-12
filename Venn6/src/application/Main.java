9911e26c21384f441f10aa06c3968cadd5349d31
devesh-ramnarine <devesh12@my.yorku.ca>
2020-02-12 13:52:43 -0500

    update

---
+++ Venn6/src/application/Main.java
@@ -0,0 +1,27 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	
+	public void start(Stage primaryStage) {
+		try {
+			Pane root =(Pane) FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root,400,400);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
+
