8de2d1ecc9ca703e842704a0b35598ed4b437a1d
devesh-ramnarine <devesh12@my.yorku.ca>
2020-02-12 13:00:33 -0500

    Initialization

---
+++ Venn3/src/application/CustomControl.java
@@ -0,0 +1,22 @@
+package application;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CustomControl {
+
+    @FXML
+    private Button button2;
+
+    @FXML
+    private Button button1;
+
+    @FXML
+    public void onSubmitClick(ActionEvent event) {
+   System.out.println("clicked me");
+
+    }
+
+}
